55e9805957060414b0cc474e1f3fced7d0fd07da
Pål Eie <PAEIE@equinor.com>
2021-04-19 12:32:20 +0200

    Changin isClose on project from string to bool

--- src/Equinor.ProCoSys.PcsBus/Topics/ProjectTopic.cs
+++ src/Equinor.ProCoSys.PcsBus/Topics/ProjectTopic.cs
@@ -5,7 +5,7 @@
         public string Plant { get; set; }
         public string ProjectName { get; set; }
         public string Description { get; set; }
-        public string IsClosed { get; set; }
+        public bool IsClosed { get; set; }
         public const string TopicName = "project";
     }
 }
