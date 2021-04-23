178d781be4aefafd30422bd944a8702925dc72f1
Pål Eie <PAEIE@equinor.com>
2021-04-23 10:31:02 +0200

    Support tag function and register changes

--- src/Equinor.ProCoSys.PcsBus/Topics/ResponsibleTopic.cs
+++ src/Equinor.ProCoSys.PcsBus/Topics/ResponsibleTopic.cs
@@ -4,8 +4,10 @@
     {
         public string Plant { get; set; }
         public string ResponsibleGroup { get; set; }
+        public string ResponsibleGroupOld { get; set; }
         public string Description { get; set; }
         public string Code { get; set; }
+        public string CodeOld { get; set; }
         public const string TopicName = "responsible";
     }
 }
--- src/Equinor.ProCoSys.PcsBus/Topics/TagFunctionTopic.cs
+++ src/Equinor.ProCoSys.PcsBus/Topics/TagFunctionTopic.cs
@@ -4,7 +4,9 @@
     {
         public string Plant { get; set; }
         public string Code { get; set; }
+        public string CodeOld { get; set; }
         public string RegisterCode { get; set; }
+        public string RegisterCodeOld { get; set; }
         public string Description { get; set; }
         public bool IsVoided { get; set; }
         public const string TopicName = "tagfunction";
