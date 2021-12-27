078df388e6e2b0f0adad3319e1bef5eeb88a4560
Tord Joranger <TORDJ@equinor.com>
2021-12-27 08:56:17 +0100

    indent

--- src/Equinor.ProCoSys.PcsBus/Topics/ChecklistTopic.cs
+++ src/Equinor.ProCoSys.PcsBus/Topics/ChecklistTopic.cs
@@ -4,18 +4,18 @@ namespace Equinor.ProCoSys.PcsServiceBus.Topics
 {
     public class ChecklistTopic
     {
-    public string Plant { get; set; }
-    public string ProjectName { get; set; }
-    public string  ChecklistId { get; set; }
-    public string TagNo { get; set; }
-    public string TagCategory { get; set; }
-    public string FormularGroup { get; set; }
-    public string FormularDiscipline { get; set; }
-    public string Responsible { get; set; }
-    public string Status { get; set; }
-    public DateTime UpdatedAt { get; set; }
-    public DateTime SignedAt { get; set; }
-    public DateTime VerifiedAt { get; set; }
-    public const string TopicName = "checklist";
+        public string Plant { get; set; }
+        public string ProjectName { get; set; }
+        public string ChecklistId { get; set; }
+        public string TagNo { get; set; }
+        public string TagCategory { get; set; }
+        public string FormularGroup { get; set; }
+        public string FormularDiscipline { get; set; }
+        public string Responsible { get; set; }
+        public string Status { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime SignedAt { get; set; }
+        public DateTime VerifiedAt { get; set; }
+        public const string TopicName = "checklist";
     }
 }
