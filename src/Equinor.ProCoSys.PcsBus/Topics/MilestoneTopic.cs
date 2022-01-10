061e741bcd33604ad39ecb7a1b90afdebfcacc1c
Tord Joranger <TORDJ@equinor.com>
2022-01-10 10:48:31 +0100

    add milestone topic

---
+++ src/Equinor.ProCoSys.PcsBus/Topics/MilestoneTopic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Equinor.ProCoSys.PcsServiceBus.Topics
+{
+    public class MilestoneTopic
+    {
+        public string Plant { get; set; }
+        public string ProjectName { get; set; }
+        public string CommPkgNo { get; set; }
+        public string McPkgNo { get; set; }
+        public string Code { get; set; }
+        public DateTime ActualDate { get; set; }
+        public bool IsSent { get; set; }
+        public bool IsAccepted { get; set; }
+        public bool IsRejected { get; set; }
+
+        public const string TopicName = "milestone";
+    }
+}
