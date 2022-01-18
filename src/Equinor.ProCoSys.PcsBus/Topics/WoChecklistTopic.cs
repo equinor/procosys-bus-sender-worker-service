4d567aa54146cc071ebeabcde74a45bd1b47ed5c
Tord Joranger <TORDJ@equinor.com>
2022-01-18 19:57:31 +0100

    add topic for WoCheckList

---
+++ src/Equinor.ProCoSys.PcsBus/Topics/WoChecklistTopic.cs
@@ -0,0 +1,14 @@
+﻿namespace Equinor.ProCoSys.PcsServiceBus.Topics
+{
+    public class WoChecklistTopic
+    {
+        public string Plant { get; set; }
+        public string ProjectName { get; set; }
+        public string WoNo { get; set; }
+        public string TagNo { get; set; }
+        public string FormularType { get; set; }
+        public string Responsible { get; set; }
+
+        public const string TopicName = "workorderchecklist";
+    }
+}
