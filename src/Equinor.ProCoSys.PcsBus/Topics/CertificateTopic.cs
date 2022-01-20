5949b4d14f64b56d3c43c9ff2e207e85adda2fb9
Kristoffer Steen <KRST@equinor.com>
2022-01-20 12:41:07 +0100

    fix(certificate): use string instead of enum for certificate type

--- src/Equinor.ProCoSys.PcsBus/Topics/CertificateTopic.cs
+++ src/Equinor.ProCoSys.PcsBus/Topics/CertificateTopic.cs
@@ -7,7 +7,7 @@ namespace Equinor.ProCoSys.PcsServiceBus.Topics
         public string Plant { get; set; }
         public string ProjectName { get; set; }
         public string CertificateNo { get; set; }
-        public CertificateType CertificateType { get; set; }
+        public string CertificateType { get; set; }
         public CertificateStatus CertificateStatus { get; set; }
         public const string TopicName = "certificate";
     }
