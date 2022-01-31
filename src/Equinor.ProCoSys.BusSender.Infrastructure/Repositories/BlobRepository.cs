61410f09e621356e90505924babe84fdd4cb292b
Christian Alejandro Hormazabal Martin <chorm@equinor.com>
2022-01-31 21:23:09 +0100

    Try to make it work in Azure - init

---
+++ src/Equinor.ProCoSys.BusSender.Infrastructure/Repositories/BlobRepository.cs
@@ -0,0 +1,23 @@
+﻿using Azure.Storage.Blobs;
+
+namespace Equinor.ProCoSys.BusSenderWorker.Infrastructure.Repositories
+{
+    public class BlobRepository
+    {
+        private BlobContainerClient _client;
+
+        public BlobRepository(string connectionString, string containerName)
+        {
+            _client = new BlobContainerClient(connectionString, containerName);
+        }
+
+        public async void Download(string pathAndFileName, string downloadPath)
+        {
+            BlobClient blobClient = _client.GetBlobClient(pathAndFileName);
+            if (await blobClient.ExistsAsync())
+            {
+                await blobClient.DownloadToAsync(downloadPath);
+            }
+        }
+    }
+}
