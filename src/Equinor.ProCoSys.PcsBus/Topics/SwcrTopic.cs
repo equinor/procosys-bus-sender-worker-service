91d427e6675b0122d0e45df5e24f9558a16b71af
Øivind Spro Heggland <OYH@equinor.com>
2022-01-31 09:13:41 +0100

    #added SWCR Topic #added SWCR Signature Tipic # updated Pcs TOpics with SWCR and SWCR signature #updated packageversion to .28

---
+++ src/Equinor.ProCoSys.PcsBus/Topics/SwcrTopic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Equinor.ProCoSys.PcsServiceBus.Topics
+{
+    public class SwcrTopic
+    {
+        public string SWCRNO { get; set; }
+        public string SWCRId { get; set; }
+        public string CommPkgNo { get; set; }
+        public string Description { get; set; }
+        public string Modification { get; set; }
+        public string Priority { get; set; }
+        public string ControlSystem { get; set; }
+        public string Contract { get; set; }
+        public string Supplier { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime EstimatedManhours { get; set; }
+        public string Node { get; set; }
+
+        public const string TopicName = "swcr";
+    }
+}
