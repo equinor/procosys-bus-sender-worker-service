34724e62a4bab40f5443276df2756ad3e28283cc
Tord Joranger <TORDJ@equinor.com>
2022-03-28 11:21:54 +0200

    simplified wochecklist topics

--- src/Equinor.ProCoSys.PcsBus/Topics/WoChecklistDeleteTopic.cs
+++ src/Equinor.ProCoSys.PcsBus/Topics/WoChecklistDeleteTopic.cs
@@ -5,13 +5,8 @@
         public string Plant { get; set; }
         public string ProjectName { get; set; }
         public string WoNo { get; set; }
-        public string TagNo { get; set; }
-        public string TagId { get; set; }
-        public string FormularType { get; set; }
-        public string FormularGroup { get; set; }
-        public string Revision { get; set; }
-        public string Responsible { get; set; }
-
+        public string ChecklistId { get; set; }
+ 
         public const string TopicName = "wochecklist_delete";
     }
 }
--- src/Equinor.ProCoSys.PcsBus/Topics/WoChecklistTopic.cs
+++ src/Equinor.ProCoSys.PcsBus/Topics/WoChecklistTopic.cs
@@ -5,10 +5,7 @@
         public string Plant { get; set; }
         public string ProjectName { get; set; }
         public string WoNo { get; set; }
-        public string TagNo { get; set; }
-        public string FormularType { get; set; }
-        public string FormularGroup { get; set; }
-        public string Responsible { get; set; }
+        public string ChecklistId { get; set; }
 
         public const string TopicName = "wochecklist";
     }
