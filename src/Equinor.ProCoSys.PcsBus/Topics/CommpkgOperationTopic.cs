d8025e764d882bd0d3edced853741a6fc33b35bf
Tord Joranger <TORDJ@equinor.com>
2022-04-25 12:56:54 +0200

    commpkg operations

---
+++ src/Equinor.ProCoSys.PcsBus/Topics/CommpkgOperationTopic.cs
@@ -0,0 +1,22 @@
+﻿namespace Equinor.ProCoSys.PcsServiceBus.Topics
+{
+    public class CommPkgOperationTopic
+    {
+        public string Plant { get; set; }
+        public string ProjectName { get; set; }
+        public string CommPkgNo { get; set; }
+        public bool InOperation { get; set; }
+        public bool ReadyForProduction { get; set; }
+        public bool MaintenanceProgram { get; set; }
+        public bool YellowLine { get; set; }
+        public bool BlueLine { get; set; }
+        public string YellowLineStatus { get; set; }
+        public string BlueLineStatus { get; set; }
+        public bool TemporaryOperationEst { get; set; }
+        public bool PmRoutine { get; set; }
+        public bool CommissioningResp { get; set; }
+        public string ValveBlindingList { get; set; }
+        public string LastUpdated { get; set; }
+
+    }
+}
