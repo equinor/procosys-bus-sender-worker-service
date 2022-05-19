4f3ed1b6fadcebeede7c53f74b43222e4a3fa5d1
Tord Joranger <TORDJ@equinor.com>
2022-05-19 15:46:02 +0200

    fix some small bugs

--- src/Equinor.ProCoSys.BusSender.Infrastructure/Repositories/DocumentRepository.cs
+++ src/Equinor.ProCoSys.BusSender.Infrastructure/Repositories/DocumentRepository.cs
@@ -9,7 +9,6 @@ namespace Equinor.ProCoSys.BusSenderWorker.Infrastructure.Repositories;
 
 public class DocumentRepository : IDocumentRepository
 {
-
     private readonly BusSenderServiceContext _context;
     private readonly ILogger<DocumentRepository> _logger;
 
@@ -36,13 +35,13 @@ public class DocumentRepository : IDocumentRepository
 
         if (!result.HasRows)
         {
-            _logger.LogError($"Document with id {documentId} did not return anything");
+            _logger.LogError("Document with id {documentId} did not return anything", documentId);
             return "{}";
         }
 
         if (!await result.ReadAsync() || result[0] is DBNull)
         {
-            _logger.LogError($"Document with id {documentId} did not return anything");
+            _logger.LogError("Document with id {documentId} did not return anything", documentId);
             return "{}";
         }
 
@@ -60,46 +59,47 @@ public class DocumentRepository : IDocumentRepository
     private static string GetQueryQuery(long documentId) =>
         @$"select
         '{{""Plant"" : ""' || q.projectschema ||
-        '"", ""QueryId"" : ""'|| do.DOCUMENT_ID ||
-        '"", ""QueryNo"" : ""'|| do.DOCUMENTNO ||
-        '"", ""Description"" : ""'|| regexp_replace(do.TITLE , '([""\])', '\\\1') ||
-        '"", ""Discipline"" : ""'|| dlib.CODE ||
-        '"", ""QueryType"" : ""'|| qtlib.CODE ||
-        '"", ""CostImpactId"" : ""'||  ci.CODE ||
+        '"", ""ProjectName"" : ""' || p.name ||
+        '"", ""QueryId"" : ""'|| do.document_id ||
+        '"", ""QueryNo"" : ""'|| do.documentno ||
+        '"", ""Description"" : ""'|| regexp_replace(do.title , '([""\])', '\\\1') ||
+        '"", ""Discipline"" : ""'|| dis.code ||
+        '"", ""QueryType"" : ""'|| qt.code ||
+        '"", ""CostImpactId"" : ""'||  ci.code ||
         '"", ""Consequence"" : ""'||  regexp_replace(q.CONSEQUENCE , '([""\])', '\\\1') ||
         '"", ""ProposedSolution"" : ""'|| regexp_replace(q.PROPOSEDSOLUTION , '([""\])', '\\\1') ||
-        '"", ""EngineeringReply"" : ""'||  regexp_replace(q.Engineeringreply, '([""\])', '\\\1') ||
-        '"", ""Milestone"" :""' || (select code
+        '"", ""EngineeringReply"" : ""'|| regexp_replace(q.Engineeringreply, '([""\])', '\\\1') ||
+        '"", ""Milestone"" :""'|| (select code
                                        from procosys.library
-                                       WHERE library_id =
-                                         (SELECT library_id
-                                          FROM procosys.elementfield fi_ex
-                                          WHERE fi_ex.ELEMENT_ID = q.DOCUMENT_ID
-                                          AND EXISTS
-                                            (SELECT 1
-                                            FROM procosys.field f
-                                            WHERE f.columnname = 'QUERY_SM'
-                                            AND f.field_id = fi_ex.field_id))) ||
-        '"", ""ScheduleImpact"" : ""'||  decode(q.SCHEDULEIMPACT,'Y', 'true', 'N', 'false') ||
-        '"", ""PossibleWarrantyClaim"" : ""'||  decode(q.POSSIBLEWARRENTYCLAIM,'Y', 'true', 'N', 'false') ||
-        '"", ""IsVoided"" : ""' || decode(e.IsVoided,'Y', 'true', 'N', 'false') ||
-        '"", ""RequiredDate"" : ""'||  TO_CHAR(q.REQUIREDREPLYDATE, 'yyyy-mm-dd hh24:mi:ss') ||
-        '"", ""CreatedAt"" :""'||  TO_CHAR(e.CREATEDAT, 'yyyy-mm-dd hh24:mi:ss') ||
+                                       where library_id =
+                                         (select library_id
+                                          from procosys.elementfield fi_ex
+                                          where fi_ex.element_id = q.document_id
+                                          and EXISTS
+                                            (select 1
+                                            from procosys.field f
+                                            where f.columnname = 'QUERY_SM'
+                                            and f.field_id = fi_ex.field_id))) ||
+        '"", ""ScheduleImpact"" : '|| decode(q.SCHEDULEIMPACT,'Y', 'true', 'false') ||
+        ', ""PossibleWarrantyClaim"" : '|| decode(q.POSSIBLEWARRENTYCLAIM,'Y', 'true', 'false') ||
+        ', ""IsVoided"" : ' || decode(e.IsVoided,'Y', 'true', 'N', 'false') ||
+        ', ""RequiredDate"" : ""'|| TO_CHAR(q.REQUIREDREPLYDATE, 'yyyy-mm-dd hh24:mi:ss') ||
+        '"", ""CreatedAt"" :""'|| TO_CHAR(e.CREATEDAT, 'yyyy-mm-dd hh24:mi:ss') ||
         '"", ""LastUpdated"" : ""'|| TO_CHAR(q.last_updated, 'yyyy-mm-dd hh24:mi:ss') ||
         '""}}' as message
         from query q
-            join document DO ON do.DOCUMENT_ID = q.DOCUMENT_ID
+            join document do ON do.document_id = q.document_id
             join element e on e.element_id = do.document_id
-            join library dlib on dlib.library_id = do.discipline_id
-            join library qtlib on qtlib.library_id = q.QUERYTYPE_ID
-            join library ci ON ci.library_id = q.COSTIMPACT_ID
-        where q.documentId = '{documentId}'";
+            left join library dis on dis.library_id = do.discipline_id
+            left join library qt on qt.library_id = q.QUERYTYPE_ID
+            left join library ci ON ci.library_id = q.COSTIMPACT_ID
+         where q.document_id = {documentId}";
 
     private static string GetDocumentQuery(long documentId) =>
-        @$"SELECT
+        @$"select
        '{{""Plant"": ""' || d.projectschema ||
        '"", ""ProjectName"" : ""' || p.name ||
-       '"", ""DocumentId"" : ""' ||d.Document_id ||
+       '"", ""DocumentId"" : ""' ||d.document_id ||
        '"", ""DocumentNo"" : ""' || d.DocumentNo ||
        '"", ""Title"" : ""' || p.name ||
        '"", ""AcceptanceCode"" : ""' || apc.code ||
@@ -117,18 +117,18 @@ public class DocumentRepository : IDocumentRepository
        '"", ""LastUpdated"" : ""' || TO_CHAR(d.Last_Updated, 'yyyy-mm-dd hh24:mi:ss') ||
        '"", ""RevisionDate"" : ""' || TO_CHAR(d.Revisiondate, 'yyyy-mm-dd hh24:mi:ss') ||
        '""}}' as message
-    from document d
-        left join library RT on RT.library_id = d.register_id
-        left join library DT on DT.library_id = d.documenttype_id
-        left join library apc on apc.library_id = d.acceptancecode_id
-        left join library dc on dc.library_id = d.documentcategory_id
-        left join library arc on arc.library_id = d.archive_id
-        left join library dp on dp.library_id = d.discipline_id
-        left join library rev on rev.library_id = d.revisionstatus_id
-        left join library ho on ho.library_id = d.handoverstatus_id
-        left join library res on res.library_id = d.responsiblecontractor_id
-        left join library acc on acc.library_id = d.accesscode_id
-        left join library com on com.library_id = d.complex_id
-        left join PROJECT p on p.project_id = d.project_id
-    where d.document_id ='{documentId}'";
+        from document d
+            join project p on p.project_id = d.project_id
+            left join library RT on RT.library_id = d.register_id
+            left join library DT on DT.library_id = d.documenttype_id
+            left join library apc on apc.library_id = d.acceptancecode_id
+            left join library dc on dc.library_id = d.documentcategory_id
+            left join library arc on arc.library_id = d.archive_id
+            left join library dp on dp.library_id = d.discipline_id
+            left join library rev on rev.library_id = d.revisionstatus_id
+            left join library ho on ho.library_id = d.handoverstatus_id
+            left join library res on res.library_id = d.responsiblecontractor_id
+            left join library acc on acc.library_id = d.accesscode_id
+            left join library com on com.library_id = d.complex_id
+        where d.document_id = {documentId}";
 }
