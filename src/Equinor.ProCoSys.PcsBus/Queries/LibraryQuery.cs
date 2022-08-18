37a3cd94267f5cb91129333a5ff18f9a36468a73
Tord Joranger <TORDJ@equinor.com>
2022-08-18 09:41:24 +0200

    voided to bool

--- src/Equinor.ProCoSys.PcsBus/Queries/LibraryQuery.cs
+++ src/Equinor.ProCoSys.PcsBus/Queries/LibraryQuery.cs
@@ -13,8 +13,8 @@ public class LibraryQuery
             '"", ""ParentId"" : ""' || regexp_replace(l.parent_id, '([""\])', '\\\1') ||
             '"", ""Code"" : ""' || regexp_replace(l.code, '([""\])', '\\\1') ||
             '"", ""Description"" : ""' || regexp_replace(l.description, '([""\])', '\\\1') ||
-            '"", ""IsVoided"" : ""'  || decode(l.isVoided,'Y', 'true', 'N', 'false') ||
-            '"", ""Type"" : ""' || regexp_replace(l.librarytype, '([""\])', '\\\1') ||
+            '"", ""IsVoided"" : ' || decode(l.isVoided,'Y', 'true', 'N', 'false') ||
+            ', ""Type"" : ""' || regexp_replace(l.librarytype, '([""\])', '\\\1') ||
             '"", ""LastUpdated"" : ""' || TO_CHAR(l.LAST_UPDATED, 'yyyy-mm-dd hh24:mi:ss') ||
             '""}}'  as message
             from library l
