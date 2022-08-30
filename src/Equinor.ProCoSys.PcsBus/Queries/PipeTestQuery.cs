deb939556fad46588705e63da0225a2ec7583e1f
Tord Joranger <TORDJ@equinor.com>
2022-08-30 10:24:04 +0200

    wip

---
+++ src/Equinor.ProCoSys.PcsBus/Queries/PipeTestQuery.cs
@@ -0,0 +1,28 @@
+﻿namespace Equinor.ProCoSys.PcsServiceBus.Queries;
+
+public class PipeTestQuery
+{
+    public static string GetQuery(long? tagCheckId, string plant = null)
+    {
+        DetectFaultyPlantInput(plant);
+        var whereClause = CreateWhereClause(tagCheckId, plant, "co", "calloff_id");
+        whereClause += "and co.calloffno is not null";
+
+        return @$"select
+        '{{""Plant"" : ""' || co.projectschema ||
+        '"", ""CallOffId"" : ""' || co.calloff_id ||
+        '"", ""CallOffNo"" : ""' || regexp_replace(co.calloffno, '([""\])', '\\\1') ||
+        '"", ""PackageId"" : ""' || co.package_id ||
+        '"", ""PurchaseOrderNo"" : ""' || regexp_replace(po.packageno, '([""\])', '\\\1') ||
+
+        '"", ""CreatedAt"" : ""' || TO_CHAR(co.createdat, 'yyyy-mm-dd hh24:mi:ss') ||
+        '""}}' as message
+        from pipetest pt
+            join purchaseorder po on po.package_id = co.package_id       
+            left join responsible r on r.responsible_id = co.responsible_id
+            left join library contractor on contractor.library_id = co.contractor_id
+            left join library supplier on supplier.library_id = co.supplier_id
+        {whereClause}";
+    }
+
+}
