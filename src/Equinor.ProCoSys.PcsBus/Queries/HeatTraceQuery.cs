d19fe468e50716daab5ff20ce83d6eb84fbcf3b3
Tord Joranger <TORDJ@equinor.com>
2022-09-14 10:56:26 +0200

    fix ht query

--- src/Equinor.ProCoSys.PcsBus/Queries/HeatTraceQuery.cs
+++ src/Equinor.ProCoSys.PcsBus/Queries/HeatTraceQuery.cs
@@ -16,8 +16,6 @@ public class HeatTraceQuery
         '"", ""LastUpdated"" : ""' || TO_CHAR(ht.last_updated, 'yyyy-mm-dd hh24:mi:ss') ||
         '""}}' as message
         from htjboxcableservice ht
-            join pipingrevision pr on pr.pipingrevision_id = pt.pipingrevision_id      
-            join pipetestlibrary ptl on ptl.pipetestlibrary_id = pt.pipetestlibrary_id  
         {whereClause}";
     }
 }
