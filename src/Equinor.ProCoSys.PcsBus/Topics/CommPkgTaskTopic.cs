a73d25836ba6e11a580fab57afe4654b7a6f6887
TWOLD <TWOLD@equinor.com>
2022-11-15 11:14:28 +0100

    Added CreateWhereClause. Indentation

--- src/Equinor.ProCoSys.PcsBus/Topics/CommPkgTaskTopic.cs
+++ src/Equinor.ProCoSys.PcsBus/Topics/CommPkgTaskTopic.cs
@@ -4,12 +4,5 @@ namespace Equinor.ProCoSys.PcsServiceBus.Topics;
 
 public class CommPkgTaskTopic
 {
-    public string Plant { get; set; }
-    public string ProjectName { get; set; }
-    public string TaskGuid { get; set; }
-    public string CommPkgGuid { get; set; }
-    public string CommPkgNo { get; set; }
-    public string LastUpdated { get; set; }
-
     public const string TopicName = "commpkgtask";
 }
