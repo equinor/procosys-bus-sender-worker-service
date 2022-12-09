fa35b0635d7f676ede882d41db6fec7342020a9a
TWOLD <TWOLD@equinor.com>
2022-12-09 08:59:00 +0100

    ActionQuery withou using RAW string

--- src/Equinor.ProCoSys.PcsBus/Queries/ActionQuery.cs
+++ src/Equinor.ProCoSys.PcsBus/Queries/ActionQuery.cs
@@ -7,26 +7,25 @@ public class ActionQuery
         DetectFaultyPlantInput(plant);
         var whereClause = CreateWhereClause(actionId, plant, "ec", "element_id");
 
-        var sql = $$"""
-            select
-               '{"Plant": "' || ec.PROJECTSCHEMA ||
-               '", "ProCoSysGuid" : "' || ec.PROCOSYS_GUID ||
-               '", "ActionNo" : "' || SUBSTR(
+        return @$"select
+           '{{""Plant"": ""' || ec.ProjectSchema ||
+           '"", ""ProCoSysGuid"" : ""' || ec.PROCOSYS_GUID ||
+           '"", ""ActionNo"" : ""' || SUBSTR(
                     (
                         SELECT LISTAGG(CODE,'.') WITHIN GROUP (ORDER BY LEVEL DESC)
                         FROM ELEMENTCONTENT n
                         START WITH n.ELEMENT_ID=ec.ELEMENT_ID
                         CONNECT BY PRIOR n.PARENT_ID=n.ELEMENT_ID
                     ),3) ||
-               '", "Title" : "' || regexp_replace(ec.Title, '(["\])', '\\\1') ||
-               '", "ElementContentGuid" : "' || ec.Id ||
-               '", "Description" : "' || regexp_replace(ec.Description, '(["\])', '\\\1') ||
-               '", "CommPkgNo" : "' || c.CommPkgNo ||
-               '", "CommPkgGuid" : "' || c.Procosys_guid ||
-               '", "LastUpdated" : "' || TO_CHAR(ec.Last_Updated, 'yyyy-mm-dd hh24:mi:ss') ||
-               '", "SignedAt" : "' || TO_CHAR(es.SignedAt, 'yyyy-mm-dd hh24:mi:ss') ||
-               '", "SignedBy" : "' || p.Azure_Oid ||
-               '"}' as message
+           '"", ""Title"" : ""' || regexp_replace(ec.Title, '([""\])', '\\\1') ||
+           '"", ""ElementContentGuid"" : ""' || ec.Id ||
+           '"", ""Description"" : ""' || regexp_replace(ec.Description, '([""\])', '\\\1') ||
+           '"", ""CommPkgNo"" : ""' || c.CommPkgNo ||
+           '"", ""CommPkgGuid"" : ""' || c.Procosys_guid ||
+           '"", ""LastUpdated"" : ""' || TO_CHAR(ec.Last_Updated, 'yyyy-mm-dd hh24:mi:ss') ||
+           '"", ""SignedAt"" : ""' || TO_CHAR(es.SignedAt, 'yyyy-mm-dd hh24:mi:ss') ||
+           '"", ""SignedBy"" : ""' || p.Azure_Oid ||
+           '""}}' as message
             FROM ELEMENTCONTENT ec
             INNER JOIN LIBRARY l ON ec.LIBRARY_ID=l.LIBRARY_ID AND 'MAINACTION'=l.CODE
             INNER JOIN ELEMENTCONTENT root ON root.ELEMENT_ID = (
@@ -39,9 +38,6 @@ public class ActionQuery
             INNER JOIN COMMPKG c ON c.COMMPKG_ID=root.ELEMENT_ID  
             LEFT JOIN elementsignature es ON es.ELEMENT_ID = ec.ELEMENT_ID
             LEFT JOIN PERSON p ON es.SIGNEDBY_ID=p.PERSON_ID                                             
-            {{whereClause}}
-            """;
-        return sql;
+            {whereClause}";
     }
-
 }
