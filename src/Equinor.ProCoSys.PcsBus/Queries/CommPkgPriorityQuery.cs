00d2fd98d91bf1ad3d3f4adc05cfd210779b49aa
Tord Joranger <TORDJ@equinor.com>
2023-01-03 14:00:58 +0100

    shell of priority query

---
+++ src/Equinor.ProCoSys.PcsBus/Queries/CommPkgPriorityQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equinor.ProCoSys.PcsServiceBus.Queries;
+
+public class CommPkgPriorityQuery
+{
+
+}
