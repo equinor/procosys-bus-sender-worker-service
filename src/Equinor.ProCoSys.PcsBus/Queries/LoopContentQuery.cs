59d5017588e586f5b5fe02dde6606a43b6f1a5bf
Tord Joranger <TORDJ@equinor.com>
2023-01-09 15:43:39 +0100

    add looptagguid

--- src/Equinor.ProCoSys.PcsBus/Queries/LoopContentQuery.cs
+++ src/Equinor.ProCoSys.PcsBus/Queries/LoopContentQuery.cs
@@ -11,6 +11,7 @@ public class LoopContentQuery
             '{{""Plant"" : ""' || lt.projectschema ||
             '"", ""ProCoSysGuid"" : ""' || lt.procosys_guid ||
             '"", ""LoopTagId"" : ""' || lt.looptag_id ||
+            '"", ""LoopTagGuid"" : ""' || t2.procosys_guid ||
             '"", ""TagId"" : ""' || lt.tag_id ||
             '"", ""TagGuid"" : ""' || t.procosys_guid ||
             '"", ""RegisterCode"" : ""' || register.code ||
@@ -18,6 +19,7 @@ public class LoopContentQuery
             '""}}'
         from looptag lt
             join tag t on t.tag_id = lt.tag_id
+            join tag t2 on t2.tag_id = lt.looptag_id
             left join library register on register.library_id = t.register_id
         {whereClause}";
     }
