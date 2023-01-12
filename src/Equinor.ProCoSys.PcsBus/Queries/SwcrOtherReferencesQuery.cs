6429c089fc441fa6c8bd76dbd80b9552c0fc9246
TWOLD <TWOLD@equinor.com>
2023-01-12 12:46:35 +0100

    Added support for SWCR Other References

---
+++ src/Equinor.ProCoSys.PcsBus/Queries/SwcrOtherReferencesQuery.cs
@@ -0,0 +1,24 @@
+﻿namespace Equinor.ProCoSys.PcsServiceBus.Queries;
+
+public class SwcrOtherReferencesQuery
+{
+    public static string GetQuery(string swcrOtherReferencesGuid, string plant= null)
+    {
+        DetectFaultyPlantInput(plant);
+        var whereClause = CreateWhereClauseForGuid(swcrOtherReferencesGuid, plant, "slr", "procosys_guid");
+
+        return @$"select
+           '{{""Plant"": ""' || slr.ProjectSchema ||
+           '"", ""ProCoSysGuid"" : ""' || slr.PROCOSYS_GUID ||
+           '"", ""LibraryGuid"" : ""' || l.PROCOSYS_GUID ||
+           '"", ""SwcrGuid"" : ""' || s.PROCOSYS_GUID ||
+           '"", ""Code"" : ""' || l.CODE ||
+           '"", ""Description"" : ""' || slr.DESCRIPTION ||
+           '"", ""LastUpdated"" : ""' || TO_CHAR(slr.LAST_UPDATED, 'yyyy-mm-dd hh24:mi:ss') ||
+           '""}}' as message
+            FROM SWCRLIBRARYREFERENCE slr
+            INNER JOIN SWCR s ON s.SWCR_ID=slr.SWCR_ID
+            INNER JOIN LIBRARY l ON slr.LIBRARY_ID=l.LIBRARY_ID
+            {whereClause}";
+    }
+}
