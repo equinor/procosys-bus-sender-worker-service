3c9759eb4e94af327fbc3a7e55e0949088933f20
Tord Joranger <TORDJ@equinor.com>
2023-06-05 15:42:23 +0200

    fix incorrect parse of guid

--- src/Equinor.ProCoSys.BusSender.Infrastructure/Handlers/GuidTypeHandler.cs
+++ src/Equinor.ProCoSys.BusSender.Infrastructure/Handlers/GuidTypeHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Text;
 using Dapper;
 
 namespace Equinor.ProCoSys.BusSenderWorker.Infrastructure.Handlers;
@@ -8,11 +9,28 @@ public class GuidTypeHandler : SqlMapper.TypeHandler<Guid>
 {
     public static readonly GuidTypeHandler Default = new();
 
-    public override Guid Parse(object value) =>
-        Guid.TryParse(value.ToString(), out var guid)
+    public override Guid Parse(object value)
+    {
+        var result = Guid.TryParse(value.ToString(), out var guid)
             ? guid
-            : new Guid((byte[])value);
+            : Guid.TryParse(ByteArrayToHexString((byte[])value), out var guid2)
+                ? guid2
+                : new Guid((byte[])value);
+        return result;
+    }
 
     public override void SetValue(IDbDataParameter parameter, Guid value)
         => throw new NotImplementedException();
+    
+    private static string ByteArrayToHexString(byte[] bytes)
+    {
+        StringBuilder result = new StringBuilder(bytes.Length * 2);
+
+        foreach (var b in bytes)
+        {
+            result.Append(b.ToString("x2"));
+        }
+
+        return result.ToString();
+    }
 }
