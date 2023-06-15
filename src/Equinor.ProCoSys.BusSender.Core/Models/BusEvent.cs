385536cda316abbb71fcd474ad87d7ce1bf34292
Tord Joranger <TORDJ@equinor.com>
2023-06-15 14:57:00 +0200

    json ignore on status field

--- src/Equinor.ProCoSys.BusSender.Core/Models/BusEvent.cs
+++ src/Equinor.ProCoSys.BusSender.Core/Models/BusEvent.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.Json.Serialization;
 
 #pragma warning disable CS8618
 
@@ -10,6 +11,8 @@ public class BusEvent
     public string Event { get; set; }
     public string Message { get; set; }
     public DateTime Created { get; init; }
+    
+    [JsonIgnore]
     public Status Status { get; set; }
 
     //untracked
