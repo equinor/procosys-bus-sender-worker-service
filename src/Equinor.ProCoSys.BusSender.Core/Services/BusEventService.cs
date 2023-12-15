5fd48b7331363f2bb8acd90422b1958ad1f5003a
Tord Joranger <TORDJ@equinor.com>
2023-12-15 14:11:26 +0100

    fix tests

--- src/Equinor.ProCoSys.BusSender.Core/Services/BusEventService.cs
+++ src/Equinor.ProCoSys.BusSender.Core/Services/BusEventService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Dapper;
 using Equinor.ProCoSys.BusSenderWorker.Core.Extensions;
 using Equinor.ProCoSys.BusSenderWorker.Core.Interfaces;
 using Equinor.ProCoSys.BusSenderWorker.Core.Models;
