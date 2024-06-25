ec3a8fa270f906674dfb30469e2a455ed241f82e
Jostein Søiland <jsoi@equinor.com>
2024-06-25 14:21:22 +0200

    Cosmetic fix

--- src/Equinor.ProCoSys.BusSender.Core/Interfaces/IBusEventService.cs
+++ src/Equinor.ProCoSys.BusSender.Core/Interfaces/IBusEventService.cs
@@ -44,6 +44,6 @@ public interface IBusEventService
     Task<string?> CreateWorkOrderMessage(string busEventMessage);
     Task<string?> CreateWorkOrderMilestoneMessage(string message);
     Task<string?> CreateHeatTracePipeTestMessage(string message);
+    Task<string?> CreateNotificationMessage(string message);
     string? WashString(string? message);
-    Task<string?> CreateNotificationMessage(string arg);
 }
