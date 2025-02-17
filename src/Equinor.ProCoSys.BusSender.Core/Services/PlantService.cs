7c58c2b1ca3ad5ce7fd968c190805b7ce2699de0
Trond Lindanger <tklin@equinor.com>
2025-02-17 15:07:14 +0100

    GH00839: Changed log level related to GetAllPlants.

--- src/Equinor.ProCoSys.BusSender.Core/Services/PlantService.cs
+++ src/Equinor.ProCoSys.BusSender.Core/Services/PlantService.cs
@@ -31,18 +31,18 @@ public class PlantService : IPlantService
     {
         if (!_cache.TryGetValue("AllPlants", out List<string>? allPlants))
         {
-            _logger.LogDebug("Retrieving plants from memory cache.");
+            _logger.LogInformation("Retrieving plants from memory cache.");
             allPlants = _plantRepository.GetAllPlants();
             _cache.Set("AllPlants", allPlants, new MemoryCacheEntryOptions
             {
                 // Have to restart instance to reload plants configuration.
                 Priority = CacheItemPriority.NeverRemove
             });
-            _logger.LogDebug("Plants read from database and added to memory cache.");
+            _logger.LogInformation("Plants read from database and added to memory cache.");
         }
         else
         {
-            _logger.LogDebug("Plants retrieved from memory cache.");
+            _logger.LogInformation("Plants retrieved from memory cache.");
         }
 
         return allPlants;
