c1c42f80014d1b60b2ba51b1f6bf8f32e3daf2a5
Trond Lindanger <tklin@equinor.com>
2025-04-28 08:58:40 +0200

    GH01676: Separation between multi and single query.

--- src/Equinor.ProCoSys.BusSender.Infrastructure/Repositories/TagDetailsRepository.cs
+++ src/Equinor.ProCoSys.BusSender.Infrastructure/Repositories/TagDetailsRepository.cs
@@ -21,12 +21,6 @@ public class TagDetailsRepository : ITagDetailsRepository
         _logger = logger;
     }
 
-    public async Task<string> GetDetailsStringByTagId(long tagId)
-    {
-        var result = await GetDetailsByTagId([tagId]);
-        return result.TryGetValue(tagId, out var details) ? details : "{}";
-    }
-
     public async Task<Dictionary<long, string>> GetDetailsByTagId(IEnumerable<long> tagIds)
     {
         var dbConnection = _context.Database.GetDbConnection();
@@ -43,7 +37,13 @@ public class TagDetailsRepository : ITagDetailsRepository
             var tagDetailsDictionary = tagIds.ToDictionary(id => id, _ => "{}");
 
             await using var command = _context.Database.GetDbConnection().CreateCommand();
-            command.CommandText = GetTagDetailsQuery(tagIds);
+            if (tagIds.Count()>1) {
+                command.CommandText = GetTagDetailsQuery(tagIds);
+            }
+            else
+            {
+                command.CommandText = GetTagDetailsQuerySingle(tagIds.First());
+            }
 
             await using var result = await command.ExecuteReaderAsync();
 
@@ -116,4 +116,45 @@ public class TagDetailsRepository : ITagDetailsRepository
             )
             group by tagId
         ";
+
+    private static string GetTagDetailsQuerySingle(long tagId) =>
+        @$"
+            select tagId, listagg('""'|| colName ||'"":""'|| regexp_replace(val, '([""\])', '\\\1') ||'""'
+            || case when colname2 is not null then ',' || '""'|| colName2 ||'"":""'|| val2 ||'""' end
+            || case when colname3 is not null then ',' || '""'|| colName3 ||'"":""'|| val3 ||'""' end,
+            ',')
+            within group (order by colName, colName2, colName3) as tagDetails
+            from (
+                select
+                val.element_id tagId,
+                decode(f.columnname, 'FROM_TAG_NUMBER', 'FromTagGuid', null) as colName2,
+                decode(f.columnname, 'TO_TAG_NUMBER', 'ToTagGuid', null) as colName3,
+                f.columnname as colName,
+                coalesce(
+                    val.valuestring,
+                    to_char(val.valuedate, 'yyyy-mm-dd hh24:mi:ss'),
+                    to_char(val.valuenumber),
+                    tag.tagno,
+                    libval.code
+                ) as val,
+                tag.procosys_guid as val2,
+                tag.procosys_guid as val3
+                from defineelementfield def
+                    left join field f on def.field_id = f.field_id
+                    left join library unit on unit.library_id = f.unit_id
+                    join elementfield val on (val.field_id = def.field_id and val.element_id = {tagId})
+                    join tag t on t.tag_id = val.element_id 
+                    left join library libval on libval.library_id = val.library_id
+                    left join library reg on reg.library_id = def.register_id
+                    left join tag on tag.tag_id = val.tag_id
+                where def.elementtype = 'TAG'
+                and (def.register_id is null or def.register_id = t.register_id)
+                and not def.isvoided = 'Y'
+                and f.columntype in ('NUMBER', 'DATE', 'STRING', 'LIBRARY', 'TAG')
+                and f.projectschema = t.projectschema
+                order by def.sortkey nulls first
+            )
+            group by tagId
+        ";
+
 }
