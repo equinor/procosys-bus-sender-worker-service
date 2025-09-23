4d990345da69986128ef5227efd0ab0cca5b2086
TORDJ <TORDJ@equinor.com>
2025-09-23 15:42:50 +0200

    order matters

--- src/Equinor.ProCoSys.BusSender.Core/Models/LibraryToLibraryEvent.cs
+++ src/Equinor.ProCoSys.BusSender.Core/Models/LibraryToLibraryEvent.cs
@@ -5,14 +5,16 @@ using Equinor.ProCoSys.PcsServiceBus.Interfaces;
 namespace Equinor.ProCoSys.BusSenderWorker.Core.Models;
 
 public sealed record LibraryToLibraryEvent(
-    Guid ProCoSysGuid,
     string Plant,
+    Guid ProCoSysGuid,
     string Role,
     string Association,
     Guid LibraryGuid,
     Guid RelatedLibraryGuid,
-    DateTime LastUpdated,
-    string EventType = PcsEventConstants.LibraryToLibraryCreateOrUpdate
-) : ILibraryToLibraryEventV1;
+    DateTime LastUpdated
+) : ILibraryToLibraryEventV1
+{
+    public string EventType => PcsEventConstants.LibraryToLibraryCreateOrUpdate;
+}
 
 
